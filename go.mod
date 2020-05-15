08c3a12b2f212c7a9cbff25d4935bd79a06e6901
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2020-05-15 00:16:09 -0300

    feat: Add usefull middleware

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.14
 require (
 	github.com/gofiber/compression v0.0.3
 	github.com/gofiber/fiber v1.9.6
+	github.com/gofiber/helmet v0.0.3
+	github.com/gofiber/logger v0.0.6
+	github.com/gofiber/recover v0.0.5
 	github.com/klauspost/compress v1.10.5 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/gofiber/compression"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/helmet"
+	"github.com/gofiber/logger"
+	"github.com/gofiber/recover"
 )
 
 const port = 8000
@@ -11,11 +14,12 @@ func main() {
 	app := fiber.New()
 
 	app.Use(compression.New())
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(helmet.New())
 
 	app.Post("/planets", create)
-
 	app.Get("/planets", index)
-
 	app.Delete("/planets/:id", delete)
 
 	app.Listen(port)
